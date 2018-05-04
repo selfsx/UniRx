d424bc6e4bd3c7d5d69578d2e60b47abfda5e68d
Sergey Ivonchik <sergey@whitesharx.com>
2018-05-04 09:38:10 +0300

    fix scene playback to initialize it separately

--- Assets/Plugins/UniRx/Scripts/UnityEngineBridge/ScenePlaybackDetector.cs
+++ Assets/Plugins/UniRx/Scripts/UnityEngineBridge/ScenePlaybackDetector.cs
@@ -1,61 +1,43 @@
-using UnityEngine;
 using UniRx.Utils;
 
 namespace UniRx {
-
-  // [InitializeOnLoad]
-  public class ScenePlaybackDetector {
-    private static bool _isPlaying = false;
-
-    private static bool AboutToStartScene {
-      get {
-        return EditorReflect.GetBool("AboutToStartScene");
-      }
-      set {
-        EditorReflect.SetBool("AboutToStartScene", value);
-      }
-    }
-
-    public static bool IsPlaying {
-      get {
-        return _isPlaying;
-      }
-      set {
-        if (_isPlaying != value) {
-          _isPlaying = value;
+  public static class ScenePlaybackDetector {
+    public static bool IsPlaying { get; private set; }
+    
+    // Place \w [InitializeOnLoad] ensures that this constructor is
+    // called when the Unity Editor is started.
+    public static void InitializeOnLoad() {
+      EditorReflect.OnPlayModeStateChanged(() => {
+        // Before scene start:          isPlayingOrWillChangePlaymode = false;  isPlaying = false
+        // Pressed Playback button:     isPlayingOrWillChangePlaymode = true;   isPlaying = false
+        // Playing:                     isPlayingOrWillChangePlaymode = false;  isPlaying = true
+        // Pressed stop button:         isPlayingOrWillChangePlaymode = true;   isPlaying = true
+        if (EditorReflect.IsPlayingOrWillChangePlaymode() && !EditorReflect.IsPlaying()) {
+          AboutToStartScene = true;
+        } else {
+          AboutToStartScene = false;
         }
-      }
+
+        // Detect when playback is stopped.
+        if (!EditorReflect.IsPlaying()) {
+          IsPlaying = false;
+        }
+      });
     }
 
-    // This callback is notified after scripts have been reloaded.
-    // [DidReloadScripts]
-    public static void OnDidReloadScripts() {
-      // Filter DidReloadScripts callbacks to the moment where playmodeState transitions into isPlaying.
+    // Place \w [DidReloadScripts] This callback is notified
+    // after scripts have been reloaded.
+    public static void DidReloadScripts() {
+      // Filter DidReloadScripts callbacks to the moment where
+      // playmodeState transitions into isPlaying.
       if (AboutToStartScene) {
         IsPlaying = true;
-      }
+      }      
     }
-
-    // InitializeOnLoad ensures that this constructor is called when the Unity Editor is started.
-    static ScenePlaybackDetector() {
-      EditorReflect.OnPlayModeStateChanged(() => {
-          // Before scene start:          isPlayingOrWillChangePlaymode = false;  isPlaying = false
-          // Pressed Playback button:     isPlayingOrWillChangePlaymode = true;   isPlaying = false
-          // Playing:                     isPlayingOrWillChangePlaymode = false;  isPlaying = true
-          // Pressed stop button:         isPlayingOrWillChangePlaymode = true;   isPlaying = true
-          if (EditorReflect.IsPlayingOrWillChangePlaymode() && !EditorReflect.IsPlaying()) 
-          {
-            AboutToStartScene = true;
-          } else {
-            AboutToStartScene = false;
-          }
-  
-          // Detect when playback is stopped.
-          if (!EditorReflect.IsPlaying()) 
-          {
-            IsPlaying = false;
-          }
-        });
-    }
+    
+    private static bool AboutToStartScene {
+      get { return EditorReflect.GetBool("AboutToStartScene"); }
+      set { EditorReflect.SetBool("AboutToStartScene", value); }
+    }    
   }
 }
